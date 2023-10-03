67c7dc1735d3743b1e21bc821d0fa77b37b7e758
unknown <omarmenjivar083@gmail.com>
2023-10-03 08:33:03 -0600

    Crando la clase Product

---
+++ src/main/java/com/proyect/agroferreteria/models/entity/Product.java
@@ -0,0 +1,76 @@
+package com.proyect.agroferreteria.models.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+//Creando la entidad Producto
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Product")
+    private Long idProduct;
+
+    @NotNull
+    private String name;
+
+    @Column(name = "unit_Weight")
+    private String unitWeight;
+
+    @NotNull
+    @Column(name = "sale_Price")
+    private Double salePrice;
+
+    private Double stock;
+
+    public Product() {
+    }
+    //getter y setter de la clase
+    public Long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnitWeight() {
+        return unitWeight;
+    }
+
+    public void setUnitWeight(String unitWeight) {
+        this.unitWeight = unitWeight;
+    }
+
+    public Double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(Double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public Double getStock() {
+        return stock;
+    }
+
+    public void setStock(Double stock) {
+        this.stock = stock;
+    }
+    //Serializaciond e la clase
+    public static long getSerializacionUID(){
+        return serializacionUID;
+    }
+    private static final long serializacionUID= 1L;
+}
