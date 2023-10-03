3c0a4c56c5be2e16b850d47243b0edd0774b1179
unknown <omarmenjivar083@gmail.com>
2023-10-02 22:11:16 -0600

    Mi segundo commit

---
+++ src/main/java/com/proyect/agroferreteria/AgroferreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.agroferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroferreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroferreteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/agroferreteria/AgroferreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.agroferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroferreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
