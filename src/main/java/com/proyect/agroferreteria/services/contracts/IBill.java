3f3dc943c071e9b35cf773cf5981393f5c7c582a
Omar Menjivar <omarmenjivar083@gmail.com>
2023-10-05 14:18:57 -0600

    Creando el contrato de itemBill y Bill

---
+++ src/main/java/com/proyect/agroferreteria/services/contracts/IBill.java
@@ -0,0 +1,14 @@
+package com.proyect.agroferreteria.services.contracts;
+
+import com.proyect.agroferreteria.models.entity.Bill;
+
+import java.util.List;
+
+public interface IBill {
+    public List<Bill> findAll();
+    public void save(Bill bill);
+    public Bill findById(Long id);
+    public void delete(Long id);
+
+
+}
---
+++ src/main/java/com/proyect/agroferreteria/services/contracts/IItemBill.java
@@ -0,0 +1,12 @@
+package com.proyect.agroferreteria.services.contracts;
+
+import com.proyect.agroferreteria.models.entity.ItemBill;
+
+import java.util.List;
+
+public interface IItemBill {
+    public List<ItemBill> findAll();
+    public void save(ItemBill itemBill);
+    public ItemBill findById(Long id);
+    public void delete(Long id);
+}
