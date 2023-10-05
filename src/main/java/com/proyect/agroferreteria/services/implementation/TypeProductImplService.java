72e8a1d6560307766543f141ff6b7b5790614c27
Omar Menjivar <omarmenjivar083@gmail.com>
2023-10-05 14:51:38 -0600

    Implementando los metodos de typeProduct

--- src/main/java/com/proyect/agroferreteria/services/implementation/TypeProductImplService.java
+++ src/main/java/com/proyect/agroferreteria/services/implementation/TypeProductImplService.java
@@ -4,30 +4,36 @@ import com.proyect.agroferreteria.models.entity.TypeProduct;
 import com.proyect.agroferreteria.repository.ITypeProduct;
 import com.proyect.agroferreteria.services.contracts.ITypeProductService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TypeProductImplService implements ITypeProductService {
-    private ITypeProduct typeProduct;
+    @Autowired
+    private ITypeProduct typeProductRepository;
     @Override
     public List<TypeProduct> findAll() {
-        return null;
+        return (List<TypeProduct>) typeProductRepository.findAll();
     }
 
     @Override
     public void save(TypeProduct typeProduct) {
-
+        if(typeProduct != null){
+            typeProductRepository.save(typeProduct);
+        }
     }
 
     @Override
     public TypeProduct findById(Long id) {
-        return null;
+        return typeProductRepository.findById(id).orElse(null);
     }
 
     @Override
     public void delete(Long id) {
-
+        if(id > 0){
+            typeProductRepository.deleteById(id);
+        }
     }
 }
