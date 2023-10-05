d506e504c9a077a7ae842a855370fa352ba657d4
Omar Menjivar <omarmenjivar083@gmail.com>
2023-10-05 14:09:27 -0600

    Creando el contrato de product

--- src/main/java/com/proyect/agroferreteria/services/contracts/ITypeProduct.java
+++ src/main/java/com/proyect/agroferreteria/services/contracts/ITypeProduct.java
@@ -1,4 +1,17 @@
 package com.proyect.agroferreteria.services.contracts;
 
+import com.proyect.agroferreteria.models.entity.Product;
+import com.proyect.agroferreteria.models.entity.TypeProduct;
+
+import java.util.List;
+
 public interface ITypeProduct {
+    public List<TypeProduct> findAll();
+    public void save(TypeProduct typeProduct);
+
+    public TypeProduct findOne(Long id);
+
+    public void delete(Long id);
+
+
 }
