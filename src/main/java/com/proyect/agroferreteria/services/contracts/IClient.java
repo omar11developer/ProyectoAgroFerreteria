4f82838a9d48fa3f0bb0bcedec600cf80d7521cf
Omar Menjivar <omarmenjivar083@gmail.com>
2023-10-05 14:21:05 -0600

    Creando el contrato de Client

---
+++ src/main/java/com/proyect/agroferreteria/services/contracts/IClient.java
@@ -0,0 +1,12 @@
+package com.proyect.agroferreteria.services.contracts;
+
+import com.proyect.agroferreteria.models.entity.Client;
+
+import java.util.List;
+
+public interface IClient {
+    public List<Client> findAll();
+    public void save(Client client);
+    public Client findById(Long id);
+    public void delete(Long id);
+}
