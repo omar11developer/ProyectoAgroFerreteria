0ef7bd3638b435703a733a8fdb93f2a20ee89bb4
Omar Menjivar <omarmenjivar083@gmail.com>
2023-10-05 12:25:52 -0600

    Creando el ropository de typeProduct

---
+++ src/main/java/com/proyect/agroferreteria/repository/ITypeProduct.java
@@ -0,0 +1,7 @@
+package com.proyect.agroferreteria.repository;
+
+import com.proyect.agroferreteria.models.entity.TypeProduct;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ITypeProduct extends CrudRepository<TypeProduct, Long> {
+}
