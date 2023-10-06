e69ded0675919fda68d186150f53fb91b407d300
unknown <omarmenjivar083@gmail.com>
2023-10-05 19:53:33 -0600

    Resolviendo la url

--- src/main/java/com/proyect/agroferreteria/controllers/userController.java
+++ src/main/java/com/proyect/agroferreteria/controllers/userController.java
@@ -1,6 +1,11 @@
 package com.proyect.agroferreteria.controllers;
 
+import com.proyect.agroferreteria.models.entity.Inventories;
+import com.proyect.agroferreteria.models.entity.TypeProduct;
 import com.proyect.agroferreteria.models.entity.Users;
+import com.proyect.agroferreteria.repository.IInventories;
+import com.proyect.agroferreteria.services.contracts.IInventoriesService;
+import com.proyect.agroferreteria.services.contracts.ITypeProductService;
 import com.proyect.agroferreteria.services.contracts.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,15 +16,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-@RestController
+@Controller
 public class userController {
     @Autowired
     private IUserService userService;
 
-    @RequestMapping(value = "/users",method = RequestMethod.GET)
-   public List<Users> userAll(){
-      return userService.findAll();
-   }
+    @GetMapping(value = "/formUser")
+    public String formSesion(Map<String, Object> model){
+        model.put("titulo", "Sesion");
+        model.put("encabezado","Iniciar Sesion");
+        return "formUser";
+    }
+
+
 
 
 
