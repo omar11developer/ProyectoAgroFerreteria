a6c9ec3a750cc5addedfca93a9bd8a8f9332b0d1
Omar Menjivar <omarmenjivar083@gmail.com>
2023-11-08 17:52:54 -0600

    Agregando gauradar y edita al controlador de proveedores

--- src/main/java/com/proyect/agroferreteria/controllers/dto/SupplierDtoController.java
+++ src/main/java/com/proyect/agroferreteria/controllers/dto/SupplierDtoController.java
@@ -4,16 +4,18 @@ import com.proyect.agroferreteria.models.dto.SupplierDTO;
 import com.proyect.agroferreteria.models.entity.Supplier;
 import com.proyect.agroferreteria.models.mapper.mapstruct.SupplierMapper;
 import com.proyect.agroferreteria.services.contracts.SupplierDAO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,7 +25,7 @@ public class SupplierDtoController extends GenericoDtoController<Supplier, Suppl
     @Autowired
     private SupplierMapper mapper;
     public SupplierDtoController(SupplierDAO service) {
-        super(service, "Provedor");
+        super(service, "Proveedor");
     }
     @GetMapping("/")
     public ResponseEntity<?> obtenerProveedores(){
@@ -42,6 +44,83 @@ public class SupplierDtoController extends GenericoDtoController<Supplier, Suppl
         response.put("data", supplierDTOS);
         return ResponseEntity.ok(response);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+        Map<String, Object> response= new HashMap<>();
+        Optional<Supplier> supplier = super.obtenerPorId(id);
+        if (supplier.isEmpty()){
+            response.put("success", Boolean.FALSE);
+            response.put("mensagge", String.format("No se encontro %s con el ID %d", nombreEntidad, id));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        Supplier supplierMapper = supplier.get();
+        SupplierDTO dto = mapper.mapSupplier(supplierMapper);
+        response.put("Success", Boolean.TRUE);
+        response.put("data", dto);
+        return ResponseEntity.ok(response);
+    }
 
+    @GetMapping("/searchByName/{name}")
+    public ResponseEntity<?> buscarPorNombre(@PathVariable String name){
+        Map<String, Object> response = new HashMap<>();
+        List<Supplier> suppliers = (List<Supplier>) service.searchByNameLikeIgnoreCase(name);
+        if(suppliers.isEmpty()){
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("No se encontro %ses con el nombre %s", nombreEntidad, name));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        List<SupplierDTO> supplierDTOS = suppliers
+                .stream()
+                .map(mapper::mapSupplier)
+                .collect(Collectors.toList());
+        response.put("success", Boolean.TRUE);
+        response.put("data", supplierDTOS);
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/")
+    public ResponseEntity<?> guardarProveedor(@Valid @RequestBody Supplier supplier, BindingResult result){
+        Map<String, Object> response = new HashMap<>();
+        boolean supplierLocal = service.existsByName(supplier.getName());
+        if(result.hasErrors()){
+            response.put("success", Boolean.FALSE);
+            response.put("validations", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(response);
+        } else if (supplierLocal) {
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("El %s que desea guardar ya existe", nombreEntidad));
+            return ResponseEntity.badRequest().body(response);
+        }
+        Supplier oSupplier = super.altaEntidad(supplier);
+        SupplierDTO dto = mapper.mapSupplier(oSupplier);
+        response.put("success", Boolean.TRUE);
+        response.put("data", dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> editarProveedor(@Valid @RequestBody Supplier supplier, @PathVariable Long id, BindingResult result){
+        Map<String, Object> response = new HashMap<>();
+        Optional<Supplier> supplierLocal = super.obtenerPorId(id);
+        Supplier supplierUpdate;
+        if (result.hasErrors()){
+            response.put("success", Boolean.FALSE);
+            response.put("validation", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(response);
+        } else if (supplierLocal.isEmpty()) {
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("El %s que desea editar no existe", nombreEntidad));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        supplierUpdate = supplierLocal.get();
+        supplierUpdate.setName(supplier.getName());
+        supplierUpdate.setCity(supplier.getCity());
+        supplierUpdate.setPhone(supplier.getPhone());
+        supplierUpdate.setAddress(supplier.getAddress());
+        Supplier save = super.altaEntidad(supplierUpdate);
+        SupplierDTO dto = mapper.mapSupplier(save);
+        response.put("success", Boolean.TRUE);
+        response.put("data", dto);
+        return ResponseEntity.ok(response);
+    }
 
 }
