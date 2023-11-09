9d172bb0c0b125c8a1f3c7237da84fc5f90ffd44
Omar Menjivar <omarmenjivar083@gmail.com>
2023-11-09 11:22:32 -0600

    Agregando el guardar a producto

--- src/main/java/com/proyect/agroferreteria/controllers/dto/ProductDtoController.java
+++ src/main/java/com/proyect/agroferreteria/controllers/dto/ProductDtoController.java
@@ -1,17 +1,19 @@
 package com.proyect.agroferreteria.controllers.dto;
 
 import com.proyect.agroferreteria.models.dto.ProductoDTO;
+import com.proyect.agroferreteria.models.entity.Category;
 import com.proyect.agroferreteria.models.entity.Product;
 import com.proyect.agroferreteria.models.mapper.mapstruct.ProductoMapper;
+import com.proyect.agroferreteria.services.contracts.CategoryDAO;
 import com.proyect.agroferreteria.services.contracts.ProductDAO;
+import com.proyect.agroferreteria.services.contracts.SupplierDAO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,9 +26,12 @@ import java.util.stream.Collectors;
 public class ProductDtoController extends GenericoDtoController<Product, ProductDAO>{
     @Autowired
     private ProductoMapper mapper;
-
-    public ProductDtoController(ProductDAO service) {
+    private final CategoryDAO categoryDAO;
+    private final SupplierDAO supplierDAO;
+    public ProductDtoController(ProductDAO service, CategoryDAO categoryDAO, SupplierDAO supplierDAO) {
         super(service, "Producto");
+        this.categoryDAO = categoryDAO;
+        this.supplierDAO = supplierDAO;
     }
     @GetMapping("/")
     public ResponseEntity<?> obtenerProductos(){
@@ -76,6 +81,35 @@ public class ProductDtoController extends GenericoDtoController<Product, Product
         response.put("data", productoDTOS);
         return ResponseEntity.ok(response);
     }
-
-
+    @PostMapping("/")
+    public ResponseEntity<?> saveProduct(@Valid @RequestBody Product product, BindingResult result){
+        Map<String, Object> response = new HashMap<>();
+        boolean productLocal = service.existeByNameProduct(product.getName());
+        boolean categoryLocal = categoryDAO.existeTypeProductName(product.getCategory().getName());
+        boolean supplierLocal = supplierDAO.existsByName(product.getSupplier().getName());
+        if(result.hasErrors()){
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(response);
+        } else if (productLocal) {
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("El nombre %s del producto que deseas agregar ya existe", product.getName()));
+            return ResponseEntity.badRequest().body(response);
+        } else if (!categoryLocal) {
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("El nombre de la categoria %s no existe", product.getCategory().getName()));
+            return ResponseEntity.badRequest().body(response);
+        } else if (!supplierLocal) {
+            response.put("success", Boolean.FALSE);
+            response.put("messagge", String.format("El nombre del proveedor %s no existe", product.getSupplier().getName()));
+            return ResponseEntity.badRequest().body(response);
+        }
+        product.setSupplier(supplierDAO.findByName(product.getSupplier().getName()));
+        product.setCategory(categoryDAO.getByName(product.getCategory().getName()));
+        Product productSave = super.altaEntidad(product);
+        ProductoDTO dto = mapper.mapProducto(productSave);
+        response.put("success", Boolean.TRUE);
+        response.put("data", dto);
+        return ResponseEntity.ok(response);
+    }
 }
